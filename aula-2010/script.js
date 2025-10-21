2042499993d3549b037509d6a63f3294dcf8dadd
Kemilly Ribeiro <kemillyraffaellavr@gmail.com>
2025-10-20 21:43:12 -0300

    add create file

---
+++ aula-2010/script.js
@@ -0,0 +1,11 @@
+function exibir(){
+    document.getElementById('texto').innerHTML = 'Seu mouse está sobre o texto';
+}
+
+function ocultar(){
+    document.getElementById('texto').innerHTML = '';
+}
+
+function elem(){
+    document.getElementById('elem').innerHTML = '';
+}
